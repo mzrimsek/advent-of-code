39a8b101d7ec8ba5f57bdff35368af6e2b24a969
Mike Zrimsek <mikezrimsek@gmail.com>
2024-12-01 13:18:04 -0500

    end day1
    need to figure out part2 off by 6 issue

--- 2023/day1/part2.js
+++ 2023/day1/part2.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 //#region input
 const input = `five8b
 2733vmmpknvgr
@@ -1004,21 +1006,55 @@ one1onermlsevenseven
 /**
  * @param {string} line - The input string to process
  */
+function convertNumberWordsToDigits(line) {
+  const numbers = {
+    'one': 1,
+    'two': 2,
+    'three': 3,
+    'four': 4,
+    'five': 5,
+    'six': 6,
+    'seven': 7,
+    'eight': 8,
+    'nine': 9
+  };
+  const convertedLine = line.replace(/one|two|three|four|five|six|seven|eight|nine/g, match => numbers[match]);
+  return convertedLine;
+}
+
+/**
+ * @param {string} line - The input string to process
+ */
+function removeNonDigits(line) {
+  return line.replace(/\D/g, '');
+}
+
+/**
+ * @param {string} line - The input string to process
+ */
 function getCalibrationValue(line) {
-  const numbers = line.match(/\d/g); // Match single digits
-  if (numbers.length === 0) {
+  const convertedLine = convertNumberWordsToDigits(line);
+  const convertedLineWithoutNonDigits = removeNonDigits(convertedLine);
+  if (convertedLineWithoutNonDigits.length === 0) {
     throw 'No numbers found!';
   }
 
-  if (numbers.length === 1) {
-    return parseInt(`${numbers[0]}${numbers[0]}`);
-  }
-  return parseInt(`${numbers[0]}${numbers[numbers.length - 1]}`);
+  const firstNumber = convertedLineWithoutNonDigits[0];
+  const lastNumber = convertedLineWithoutNonDigits[convertedLineWithoutNonDigits.length - 1];
+  const calibrationValue = parseInt(`${firstNumber}${lastNumber}`);
+  return {
+    line,
+    convertedLine,
+    convertedLineWithoutNonDigits,
+    calibrationValue
+  };
 }
 
 const lines = input.split('\n');
 const calibrationValues = lines.map(getCalibrationValue);
-const sum = calibrationValues.reduce((acc, value) => acc + value, 0);
+// const json = JSON.stringify(calibrationValues, null, 2);
+// fs.writeFileSync('calibrationValues.json', json);
+const sum = calibrationValues.reduce((acc, value) => acc + value.calibrationValue, 0);
 console.log(sum);
 
-// answer is 55029
+// answer is 55686
